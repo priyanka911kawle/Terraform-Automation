ef31e9f1b2427e09ce52b1aeb6a2d81d97ba6826
priyanka kawle <priyanka911.kawle@gmail.com>
2022-12-12 15:54:28 +0530

    backend.tfv3

--- main.tf
+++ main.tf
@@ -47,7 +47,8 @@ resource "aws_instance" "myFirstInstance" {
   key_name = var.key_name
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.jenkins-sg-2022.id]
-  tags= {
+  subnet_id = "subnet-0c2697f58390ad26d"
+    tags= {
     Name = var.tag_name
   }
 }
