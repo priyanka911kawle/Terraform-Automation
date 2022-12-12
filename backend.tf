5d384ea1c690dee9af20c56d13ad25ff92c545c6
priyanka kawle <priyanka911.kawle@gmail.com>
2022-12-12 12:48:08 +0530

    backendfile

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-2"
+    bucket = "d-s3-terraform"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
